ba7474a49de27a86392f0d544d12e33179ef6c09
RamblerSY <younger911@gmail.com>
2025-06-06 16:49:14 +0800

    Update part2.sh

--- lede/part2.sh
+++ lede/part2.sh
@@ -16,41 +16,6 @@
 #limitations under the License.
 #
 
-# Alter default kernel version
-# sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/bcm53xx/Makefile
-# cat target/linux/bcm53xx/Makefile |grep KERNEL_PATCHVER
-# echo '=========Alert Kernel OK!========='
-
-# Alter default router IP
+# Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-#修改NTP设置
-#sed -i "s/'0.openwrt.pool.ntp.org'/'ntp1.aliyun.com'/g" package/base-files/files/bin/config_generate
-#sed -i "s/'1.openwrt.pool.ntp.org'/'ntp2.aliyun.com'/g" package/base-files/files/bin/config_generate
-#sed -i "s/'2.openwrt.pool.ntp.org'/'ntp3.aliyun.com'/g" package/base-files/files/bin/config_generate
-#sed -i "s/'3.openwrt.pool.ntp.org'/'ntp4.aliyun.com'/g" package/base-files/files/bin/config_generate
-#cat package/base-files/files/bin/config_generate |grep system.ntp.server=
-#echo 'Alert NTP Settings OK!====================='
-
-echo '修改主机名'
-sed -i "s/hostname='OpenWrt'/hostname='Phicomm-K3'/g" package/base-files/files/bin/config_generate
-cat package/base-files/files/bin/config_generate |grep hostname=
-echo '=========Alert hostname OK!========='
-
-echo '移除主页跑分信息显示'
-sed -i 's/ <%=luci.sys.exec("cat \/etc\/bench.log") or ""%>//g' package/lean/autocore/files/arm/index.htm
-echo '=========Remove benchmark display in index OK!========='
-
-echo '移除主页日志打印'
-sed -i '/console.log(mainNodeName);/d' package/lean/luci-theme-argon/htdocs/luci-static/argon/js/script.js
-echo '=========Remove log print in index OK!========='
-
-echo '修改upnp绑定文件位置'
-sed -i 's/\/var\/upnp.leases/\/tmp\/upnp.leases/g' feeds/packages/net/miniupnpd/files/upnpd.config
-cat feeds/packages/net/miniupnpd/files/upnpd.config |grep upnp_lease_file
-echo '=========Alert upnp binding file directory!========='
-
-#添加主页的CPU温度显示
-#sed -i "/<tr><td width=\"33%\"><%:Load Average%>/a \ \t\t<tr><td width=\"33%\"><%:CPU Temperature%></td><td><%=luci.sys.exec(\"sed 's/../&./g' /sys/class/thermal/thermal_zone0/temp|cut -c1-4\")%></td></tr>" feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-#cat feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm |grep Temperature
-#echo "Add CPU Temperature in Admin Index OK====================="
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
